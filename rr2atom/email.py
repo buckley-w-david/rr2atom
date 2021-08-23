f4e5ad6643bcef75091cd25abdd882d43fe5e5eb
David Buckley <david@davidbuckley.ca>
2021-08-23 18:28:44 -0400

    resolve email tracking link to real url

--- rr2atom/email.py
+++ rr2atom/email.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 import email
 import re
+import urllib.request
 
 from bs4 import BeautifulSoup  # type: ignore
 from imapclient import IMAPClient  # type: ignore
@@ -8,6 +9,7 @@ from imapclient import IMAPClient  # type: ignore
 from rr2atom.models import UpdateEmail
 
 URL_PATTERN = re.compile(r"(?P<url>https?://[^\s]+)")
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
 
 
 def fetch_unprocessed(imap_client):
@@ -27,7 +29,14 @@ def fetch_unprocessed(imap_client):
         soup = BeautifulSoup(email_message.get_payload(1).get_payload(), "html.parser")
 
         body = email_message.get_payload(0).get_payload()
-        chapter_url = soup.find("a", {"data-color": "Button Link"}).get("href")
+        email_url = soup.find("a", {"data-color": "Button Link"}).get("href")
+
+        # While I'm at it, lets resolve the email tracking redirect
+        req = urllib.request.Request(
+            email_url, data=None, headers={"User-Agent": USER_AGENT}
+        )
+        with urllib.request.urlopen(req) as f:
+            chapter_url = f.url
 
         body = URL_PATTERN.sub(chapter_url, body, count=1).replace("\r", "")
         yield UpdateEmail(envelope=envelope, plaintext_content=body)
