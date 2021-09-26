2e6966f45090755f7141a517aa4f9717d6da2bbb
David Buckley <david@davidbuckley.ca>
2021-09-26 16:38:22 +0000

    revamp email parsing - RR has fixed their broken text/plain email links

--- rr2atom/email.py
+++ rr2atom/email.py
@@ -1,16 +1,21 @@
 from datetime import datetime, timedelta
 import email
+import logging
 import re
 import urllib.request
+import urllib.error
 
-from bs4 import BeautifulSoup  # type: ignore
 from imapclient import IMAPClient  # type: ignore
 
 from rr2atom.models import UpdateEmail
 
 URL_PATTERN = re.compile(r"(?P<url>https?://[^\s]+)")
+TRACKING_PATTERN = re.compile(r"https://email-click.royalroad.com/\w+/\w+")
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def fetch_unprocessed(imap_client):
     messages = imap_client.search(
@@ -18,32 +23,24 @@ def fetch_unprocessed(imap_client):
     )
     for uid, data in imap_client.fetch(messages, ["ENVELOPE", "RFC822"]).items():
         envelope = data[b"ENVELOPE"]
-        # *sigh*
-        # I would love to not bother with the html version at all
-        # Originally I fetched BODY[1] instead of RFC822
-        # Unfortunatly Royal Road's notification email system is a bit bugged
-        # and their text/plain versions have broken chapter links
-        # I really like the simplicity of the text/plain version
-        # so we're hackin' the correct link in.
         email_message = email.message_from_bytes(data[b"RFC822"])
 
-        # Quoted-printable bit me in the ass, gotta decode the payload
-        # We're just assuming that it's UTF-8 encoded, this will likely bite me in the ass
-        # at some future point too
-        html_payload = (
-            email_message.get_payload(1).get_payload(decode=True).decode("utf-8")
-        )
-        soup = BeautifulSoup(html_payload, "html.parser")
-
-        email_url = soup.find("a", {"data-color": "Button Link"}).get("href")
-
-        # While I'm at it, lets resolve the email tracking redirect
-        req = urllib.request.Request(
-            email_url, data=None, headers={"User-Agent": USER_AGENT}
-        )
-        with urllib.request.urlopen(req) as f:
-            chapter_url = f.url
-
         body = email_message.get_payload(0).get_payload(decode=True).decode("utf-8")
-        body = URL_PATTERN.sub(chapter_url, body, count=1).replace("\r", "")
+        if match := URL_PATTERN.search(body):
+            email_url = match.group("url")
+            try:
+                # While I'm at it, lets resolve the email tracking redirect
+                req = urllib.request.Request(
+                    email_url, data=None, headers={"User-Agent": USER_AGENT}
+                )
+                with urllib.request.urlopen(req) as f:
+                    chapter_url = f.url
+
+                body = URL_PATTERN.sub(chapter_url, body, count=1).replace("\r", "")
+                body = TRACKING_PATTERN.sub("", body)
+            except urllib.error.HTTPError as e:
+                # Skip old broken update link emails
+                logger.warning(e)
+                continue
+
         yield UpdateEmail(envelope=envelope, plaintext_content=body)
